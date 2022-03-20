9baf54169a480994e34364f2bd6b3134e06a71e0
brbrsuraj <paudel.suraj76@gmail.com>
2022-03-20 11:09:03 +0545

    User Seeder and RoleSeeder updated

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -16,6 +16,7 @@ class RoleSeeder extends Seeder
     {
         //
         DB::table('roles')->insert([
+            ['role'=>'supAdmin'],
             ['role'=>'admin'],
              ['role'=>'member'],
         ]);
